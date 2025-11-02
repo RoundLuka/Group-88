be00b322bbebd44b40d48e408bf2575c8ecda727
RoundLuka <lukagurgenidze282007@gmail.com>
2025-11-02 23:34:54 +0400

    commit

---
+++ Level 16/Classwork/main.py
@@ -0,0 +1,22 @@
+# დათვალე 20-იდან 40-ის ჩათვლით ყველა რიცხვის ჯამი
+
+# start = 20
+# end = 40
+
+# total = 0
+
+# for ციკლის გამოყენების არ გვიწევს საიტერაციო ცვლადის ცალკე დეკლარაცია
+
+# for num in range(start, end + 1):
+#     total += num
+
+# print(f"Sum of numbers between 20 and 41 is: {total}")
+
+# for symbol in "%$@^@$^@#$#$^(^$)":
+#     print(symbol * 2)
+
+for i in range(1, 10):
+    value1 = i % 2 == 0
+    value2 = i > 5
+    print(value1 and value2)
+
