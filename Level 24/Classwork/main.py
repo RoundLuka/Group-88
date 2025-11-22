a9877a5617fbeb780aad5f42ed02a6056c636369
RoundLuka <lukagurgenidze282007@gmail.com>
2025-11-22 23:14:22 +0400

    commit

---
+++ Level 24/Classwork/main.py
@@ -0,0 +1,61 @@
+#         -3     -2         -1
+# queue = ["Luka", "Nika", "Irakli"]
+
+# print(queue[-3])
+
+# advanced
+
+# data = [45, 5, 8, 45, 12, 43, 12, 52, 65, 785, 56, 65,34]
+
+# print(data[::-1])
+
+# ფუნქცია არის ბრძანება რომელიც ასრულებს კონკრეტულ დავალებას
+
+# ყველა ფუნქციას აქვს სახელი და როდესაც ვიყენებთ ვუწერთ სახელის ბრჩხილბს - სახელი()
+
+# პითონში ჩაშნებული ფუნქციები
+# print() - გადადაცემა უამრავი არგუმენტი, რომელსაც დაბეჭდავს, ხოლო თუ თქვენ არგუმენტს მიუთითებთ end-ს მაშინ print ყოველ სტრქინონს დაასრულეს end-ით
+
+# print()
+# input()
+
+# type()
+# range()
+
+# int()
+# float()
+# str()
+
+# line 1
+# line 2
+# line 3
+# line 4
+
+# print()
+
+# for char in "Luka":
+#     print()
+#     if char == "k":
+#         continue
+
+# print(abs(-2))
+
+# data = [5, 6, 3, 12]
+# # print(sum([5, 6, "3", 12]))
+# # print(min(data))
+# # print(max(data))
+# # length = len([True, 34, 123, "rwer", 345, 34])
+# # print(round(12.6564, 0))
+
+# import math
+
+# print(math.pow(3, 4))
+# print(math.sqrt(16))
+# print(math.floor(12.9999999))
+# print(math.ceil(5.001))
+
+import random
+
+# random.randrange(2, 10)
+print(random.seed(42))
+
