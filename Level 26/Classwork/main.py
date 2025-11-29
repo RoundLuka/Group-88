727ef4ad057459888700b96513a74ca783f3a337
RoundLuka <lukagurgenidze282007@gmail.com>
2025-11-29 23:11:52 +0400

    commit

--- Level 26/Classwork/main.py
+++ Level 26/Classwork/main.py
@@ -1,30 +1,30 @@
-# from turtle import *
+from turtle import *
 
 
-# def draw_square(x, y):
+def draw_square(x, y):
 
-#     penup()
-#     goto(x, y)
-#     pendown()
+    penup()
+    goto(x, y)
+    pendown()
 
-#     forward(200)
-#     left(90)
-#     forward(200)
-#     left(90)
-#     forward(200)
-#     left(90)
-#     forward(200)
+    forward(200)
+    left(90)
+    forward(200)
+    left(90)
+    forward(200)
+    left(90)
+    forward(200)
 
-#     left(90)
+    left(90)
 
-#     penup()
-#     goto(0, 0)
-#     pendown()
+    penup()
+    goto(0, 0)
+    pendown()
 
 
-# draw_square(200, 200)
-# draw_square(-200, 200)
-# draw_square(-200, -200)
-# draw_square(200, -200)
+draw_square(200, 200)
+draw_square(-200, 200)
+draw_square(-200, -200)
+draw_square(200, -200)
 
-# exitonclick()
+exitonclick()
---
+++ Level 28/Classwork/main.py
@@ -0,0 +1,65 @@
+# ფუნქცია - ხელახლა გამოყენბადი კოდის ბლოკი
+
+# def greeting(first_name, last_name):
+#     print("Hello " + first_name + " " + last_name)
+
+# greeting("Luka", "Gurgenidze")
+
+
+# list.append(element) - ელემენტს ამატებს სიის ბოლოს
+# nums.append(50)
+# nums.append("wrtye")
+
+# list.insert(index, element) - ელემენტს ამატებს სიაში მოცემულ ინდექსზე
+# nums.insert(2, "25")
+
+# list.extend(sequence) - ცალცალკე ამატებს დალაგებული მონცამეთა ტიპის წევრებს სიაში
+# nums.extend([70, 75, 80])
+
+# list.pop(index) - შლის მოცემულ ინდექსზე მყოფ ელემენტს
+# nums.pop() - ბოლო ელემენტს წაშლის
+
+# nums.remove(value) - წაშლის სიიდან კონკრეტულ მნიშვნლობას
+# nums.remove(30)
+
+# range(4) - 0, 1, 2, 3
+
+# odds = []
+
+# for index in range(len(nums)):
+    
+#     if index % 2 == 1:
+#         odds.append(nums[index])
+
+# nums = [55, 12, 25, 26, 26, 3246, 34]
+
+# list.sort() - ზრდადობით ალაგებს რიცხვებს და სტრქიონებს ანბანის მიხედვით, ცვილს გადაცემულ სიას
+# sorted(list) - ანალოგიურად ახარისხებს სიას, ოღონდ არ ცვლის თავდაპირველ სიას და გვაძლევს ახალ სიას
+
+# print(nums)
+
+# print("Hello".lower())
+# print("Hello".upper())
+# print("gherqREWTW".capitalize())
+# print("etgQfgwr".find("Q"))
+
+# colors = ["Red", "Blue", "Green", "Yellow", "Black"]
+# print(colors[3])
+
+# ფუნქციებს ვყოფთ 2 კატეგორიად:
+# 1. დავალებას ასრულებენ
+# 2. გამოთვლიან შედეგს
+
+# return - ფუქნციის 
+
+# def add_nums(start, stop):
+#     total = 0
+#     for num in range(start, stop):
+#         total += num
+#     return total
+
+# print(add_nums(5, 10))
+
+# coding
+# programming
+
