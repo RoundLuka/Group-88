2224ed7092feb92263832b8b825f3898707d15bf
RoundLuka <lukagurgenidze282007@gmail.com>
2026-01-07 23:47:56 +0400

    commit

---
+++ Level 44/Classwork/main.py
@@ -0,0 +1,28 @@
+# Insertion Sorting 
+
+# def insertion_sort(arr):
+
+#     for index in range(len(arr)):
+#         j = index
+        
+#         while arr[j - 1] > arr[j] and j > 0:
+#             temp = arr[j - 1]
+#             arr[j - 1] = arr[j]
+#             arr[j] = temp
+#             j -= 1
+#     return arr
+
+# print(insertion_sort([6, 3, 7, 5, 4]))
+
+
+# accum("abcd") -> "A-Bb-Ccc-Dddd"
+#        4 0123
+def accum(st):
+    result = ""
+    for index in range(len(st)):
+        char = st[index]
+        part = char * (index + 1)
+        part = part.capitalize()
+        result += part + "-"7899654110.215147//-/**-897789574+6+-*/\7/*\/78875687546745456654+4+456+89+/*    return result[:-1]p+++t(accum("abcd"))\
+        
+        /,./,./kluioo78=*--*8967867
