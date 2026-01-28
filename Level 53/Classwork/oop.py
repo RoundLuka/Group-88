ddb0c73e0f5538120e9718718bbac81aac4018ca
RoundLuka <lukagurgenidze282007@gmail.com>
2026-01-28 23:40:56 +0400

    commit

--- Level 53/Classwork/oop.py
+++ Level 53/Classwork/oop.py
@@ -27,3 +27,53 @@
 # doctor.heal_up()
 
 # print(samurai.health)
+
+# კლასი ზოგადი ცნება, რომლითაც ვაგებთ ერთგვარ ელემენტებს
+class Gamer:
+    count = 0
+    def __init__(self, game, time_played):
+        self.favourite_game = game
+        self.hours = time_played
+        Gamer.count += 1
+    
+    def play(self):
+        self.hours += 1
+
+    @classmethod
+    def count_gamers(cls):
+        print(cls.count)
+
+# კლასის კონკრეტულ წარმომადგენებლს ვუწოდებთ ინსტანციებს/ობიექტი
+# irakli = Gamer("RD2", 100)
+# luka = Gamer("Warband", 300)
+# zurabi = Gamer("Roblox", 200)
+
+# luka.play()
+# luka.play()
+# luka.play()
+
+# luka.play()
+# Gamer.play(luka)
+
+# "wetwtwtw".lower()
+
+# def any():
+#     pass
+
+# print(any)
+
+class Fighter:
+    def __init__(self, health, damage, speed):
+        self.health123 = health + 100
+        self.damage = damage
+        self.speed = speed
+
+class Archer(Fighter):
+    def __init__(self, health, damage, speed, range, reload):
+        super().__init__(health, damage, speed)
+        self.range = range
+        self.reload = reload
+
+wizard = Archer(30, 50, 20, 100, 22)
+
+print(wizard.health123)
