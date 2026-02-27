c1838d4831c23587a300d36ddff703e793507e6a
RoundyLuka <lukagurgenidze282007@gmail.com>
2026-02-27 23:58:24 +0400

    commit

--- Level 66/Classwork/script.js
+++ Level 66/Classwork/script.js
@@ -1,116 +1,116 @@
-// ecmascript 6 objects
+ecmascript 6 objects
 
-// let lastName = "username"
+let lastName = "username"
 
-// function deposit(amount) {
-//     return this.balance += amount
-// }
+function deposit(amount) {
+    return this.balance += amount
+}
 
-// reference
-
-
-// let account1 = {
-//     username: "Luka",
-//     score: 20
-// }
-
-// let account1Duplicate = Object.assign({}, account1)
-
-// console.log(account1)
-
-// account1Duplicate.username = "John"
-
-// console.log(account1Duplicate)
-
-// let acccount2 = {
-//     balance: 200
-// }
-
-// let account3 = {
-//     email: "luka@gmail.com",
-//     username: "Irakli"
-// }
-
-// let combined = Object.assign(account3, acccount2, account1)
-// console.log(combined)
+reference
 
 
-// account.deposit(25)
-// account.deposit(50)
+let account1 = {
+    username: "Luka",
+    score: 20
+}
 
-// console.log(account)
+let account1Duplicate = Object.assign({}, account1)
 
-// es6 arrow functions
+console.log(account1)
 
-// function fullName(fname, lname) {
-//     return fname + " " + lname
-// }
+account1Duplicate.username = "John"
 
-// const fullName = fname => fname + "!"
+console.log(account1Duplicate)
 
-// console.log(fullName("Luka", "Gurgenidze"))
+let acccount2 = {
+    balance: 200
+}
 
-// es6 destrucutring
+let account3 = {
+    email: "luka@gmail.com",
+    username: "Irakli"
+}
 
-// const dataArr = ["weryhretyq", "yerye", "gfjrtjyut"];
-
-// let [data1, data2, data3="duherturur", data4="eryuerue"] = dataArr;
-
-// console.log(data1)
-// console.log(data2)
-// console.log(data3)
-
-// console.log(dataArr)
-
-// const personAccount = {
-//     username: "Luka",
-//     dinosaur: false,
-//     height: 1.77
-// }
-
-// const {username: firstName, dinosaur, height} = personAccount
+let combined = Object.assign(account3, acccount2, account1)
+console.log(combined)
 
 
-// console.log(firstName)
-// console.log(dinosaur)
-// console.log(height)
+account.deposit(25)
+account.deposit(50)
 
-// let height, dinosaur, username;
-// ({height, dinosaur, username} = personAccount);
-// console.log(personAccount)
+console.log(account)
+
+es6 arrow functions
+
+function fullName(fname, lname) {
+    return fname + " " + lname
+}
+
+const fullName = fname => fname + "!"
+
+console.log(fullName("Luka", "Gurgenidze"))
+
+es6 destrucutring
+
+const dataArr = ["weryhretyq", "yerye", "gfjrtjyut"];
+
+let [data1, data2, data3="duherturur", data4="eryuerue"] = dataArr;
+
+console.log(data1)
+console.log(data2)
+console.log(data3)
+
+console.log(dataArr)
+
+const personAccount = {
+    username: "Luka",
+    dinosaur: false,
+    height: 1.77
+}
+
+const {username: firstName, dinosaur, height} = personAccount
 
 
-// spread
+console.log(firstName)
+console.log(dinosaur)
+console.log(height)
 
-// const values = [11, 36, 67]
+let height, dinosaur, username;
+({height, dinosaur, username} = personAccount);
+console.log(personAccount)
 
-// const numbers = [52, 236, 35, ...values, 37, 264]
 
-// console.log(numbers)
+spread
 
-// const [num1, num2, ...others] = numbers
+const values = [11, 36, 67]
 
-// console.log(num1)
-// console.log(num2)
-// console.log(others)
+const numbers = [52, 236, 35, ...values, 37, 264]
 
-// *args
+console.log(numbers)
 
-// rest
+const [num1, num2, ...others] = numbers
 
-// function anything(argument1, ...otherArguments) {
-//     console.log(`First: ${argument1}`)
-//     console.log(`Others: ${otherArguments}`)
-// }
+console.log(num1)
+console.log(num2)
+console.log(others)
 
-// anything("twryer", true, 2345, "erwtyer")
+*args
 
-// function magic(...nums) {
-//   let sum = 0;
-//   nums.filter(n => n % 2 == 0).map(el => sum+= el);
-//   return sum;
-// }
-// console.log(magic(1, 2, 3, 4, 5, 6));
+rest
+
+function anything(argument1, ...otherArguments) {
+    console.log(`First: ${argument1}`)
+    console.log(`Others: ${otherArguments}`)
+}
+
+anything("twryer", true, 2345, "erwtyer")
+
+function magic(...nums) {
+  let sum = 0;
+  nums.filter(n => n % 2 == 0).map(el => sum+= el);
+  return sum;
+}
+console.log(magic(1, 2, 3, 4, 5, 6));
 
 // function construcotr
 
