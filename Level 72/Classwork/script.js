7257f094e06f5a09cebb43c09bef6497e2e1512c
RoundyLuka <lukagurgenidze282007@gmail.com>
2026-03-11 23:43:28 +0400

    commit

---
+++ Level 72/Classwork/script.js
@@ -0,0 +1,16 @@
+// m = [
+//     [11, 12, 13],
+//     [21, 22, 23],
+// ]
+
+
+// var transpose = m => m[0].map((_, i) => m.map(r => r[i]));
+
+
+// ternary operator
+
+let x = 4;
+
+let isGood = x > 3 ? "good" : "not good"
+
+console.log(isGood)
