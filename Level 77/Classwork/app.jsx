98fc3424f7b077f8ad41d454040fffdff8a8b9de
RoundyLuka <lukagurgenidze282007@gmail.com>
2026-03-23 23:22:33 +0400

    commit

---
+++ Level 77/Classwork/app.jsx
@@ -0,0 +1,40 @@
+// 2) შექმენით კომპონენტი, გექნებათ ცვლადი 50/50-ზე math.random-ის დახმარებით რომლის მიხედვითაც საიტზე გამოიტანთ ან ფორმას ან დაულაგებელ სიას სადაც იქნება ღილაკი. როდესაც მოხდება ფორმის ღილაკზე / დაუალგებელ სიის ღილაკზე დაჭერა უნდა გამოიძახეთ handler ფუნქცია (უნდა გქონდეთ ორივე ღილაკისთვის სადასხვა handleSubmit, handleClick) alert-ის გამოიტანეთ რაიმე და თქვენი ცოდნით კიდევ თუ შეძლებ რაიმე ლოგია გაუწერეთ
+
+function App () {
+  const signedIn = Math.random() > 0.5;
+  
+  function handleSubmit (e) {
+    e.preventDefault();
+    alert("Form submitted")
+  }
+
+  function handleClick () {
+    alert("Clicked")
+  }
+
+
+  if (signedIn) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2>{`SignedIn: ${signedIn}`}</h2>
+        <input placeholder="Username" type="text" />
+        <button type="submit">Send</button>
+      </form>
+    )
+  } else {
+    return (
+      <div>
+        <h2>{`SignedIn: ${signedIn}`}</h2>
+        <ul>
+          <li>Georgia</li>
+          <li>Germany</li>
+          <li>Greenland</li>
+        </ul>
+        <button onClick={handleClick}>Click me</button>
+      </div>
+    )
+  }
+}
+
+
+export default App;
