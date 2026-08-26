ef78d0873803a0a9649c34d86042054602d7e97d
RoundyLuka <lukagurgenidze282007@gmail.com>
2026-08-26 23:59:51 +0400

    commit

---
+++ Randomizer11.py
@@ -0,0 +1,21 @@
+import random
+
+students = [
+    "დაჩი თეთრაძე",
+    "ქეთი ბაიდაური",
+    "ირაკლი გერაძე",
+    "გიორგი მეგრელი",
+    "ნიკოლოზ ნადირაძე",
+]
+
+captain1 = ["ნიკა კანკია",]
+captain2 = ["ზურაბ შენგელია",]        
+
+random.shuffle(students)
+
+for i in range(3):
+    if students: captain1.append(students.pop())
+    if students: captain2.append(students.pop())
+
+# print("პირველი გუნდი " + str(captain1))
+# print("მეორე გუნდი " + str(captain2))
