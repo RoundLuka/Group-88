bb03293c9b64f3e0f25d0e124c8f6d5960541977
RoundyLuka <lukagurgenidze282007@gmail.com>
2026-08-27 23:59:45 +0400

    commit

--- Randomizer11.py
+++ Randomizer11.py
@@ -17,5 +17,5 @@ for i in range(3):
     if students: captain1.append(students.pop())
     if students: captain2.append(students.pop())
 
-# print("პირველი გუნდი " + str(captain1))
-# print("მეორე გუნდი " + str(captain2))
+print("პირველი გუნდი " + str(captain1))
+print("მეორე გუნდი " + str(captain2))
