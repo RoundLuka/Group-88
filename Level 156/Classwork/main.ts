a15d2fddd71ba9cbfdc16921cdbf815d3db4ed90
RoundyLuka <lukagurgenidze282007@gmail.com>
2026-09-21 23:54:54 +0400

    commit

---
+++ Level 156/Classwork/main.ts
@@ -0,0 +1,16 @@
+// function doSomething(num1?: number): void {
+//     if (num1) {
+//         console.log(num1)
+//     }
+//     console.log("No number")
+// } 
+
+// doSomething()
+
+// type numStr = string | number | boolean;
+
+// let username: numStr = "Luka";
+// username = 5
+
+const numArr: [number, number, number, string] = [2, 5, 1, 'wetwe'];
+
